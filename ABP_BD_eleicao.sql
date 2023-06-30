e54f4bff704eea6bd5e0915d61b52263e49bf143
Max Willian <87044552+MaxWillianm@users.noreply.github.com>
2023-06-30 00:18:24 -0300

    add cd_voto_candidato

--- ABP_BD_eleicao.sql
+++ ABP_BD_eleicao.sql
@@ -1,108 +1,91 @@
-CREATE DATABASE eleicaoABP
-USE eleicaoABP ;
+
+CREATE TABLE eleicaoabp DEFAULT CHARACTER SET utf8mb4 ;
+USE eleicaoabp ;
+
+CREATE TABLE eleicaoabp.instituicao (
+  id_instituicao INT(11) NOT NULL,
+  cep_instituicao VARCHAR(8) NULL DEFAULT NULL,
+  cidade_instituicao VARCHAR(200) NULL DEFAULT NULL,
+  estado_instituicao VARCHAR(200) NULL DEFAULT NULL,
+  pais_instituicao VARCHAR(200) NULL DEFAULT NULL,
+  endereco_instiuticao VARCHAR(200) NULL DEFAULT NULL,
+  razao_social_instituicao VARCHAR(200) NULL DEFAULT NULL,
+  nome_fantasia_instituicao VARCHAR(200) NULL DEFAULT NULL,
+  cnpj_instituicao VARCHAR(14) NULL DEFAULT NULL,)
 
 
-CREATE TABLE instituição (
-  id_instituicao INT NOT NULL,
-  cep_instituicao VARCHAR(8) NULL,
-  cidade_instituicao VARCHAR(200) NULL,
-  estado_instituicao VARCHAR(200) NULL,
-  pais_instituicao VARCHAR(200) NULL,
-  endereco_instiuticao VARCHAR(200) NULL,
-  razao_social_instituicao VARCHAR(200) NULL,
-  nome_fantasia_instituicao VARCHAR(200) NULL,
-  cnpj_instituicao VARCHAR(14) NULL,
-  PRIMARY KEY (id_instituicao))
-
-
-
-CREATE TABLE eleicao (
-  id_eleicao INT NOT NULL,
+CREATE TABLE eleicaoabp.eleicao (
+  id_eleicao INT(11) NOT NULL,
   tipo_eleicao VARCHAR(200) NOT NULL,
-  cidade_eleicao VARCHAR(200) NULL,
-  bairro_eleicao VARCHAR(200) NULL,
-  estado_eleicao VARCHAR(200) NULL,
-  pais_eleicao VARCHAR(200) NULL,
+  cidade_eleicao VARCHAR(200) NULL DEFAULT NULL,
+  bairro_eleicao VARCHAR(200) NULL DEFAULT NULL,
+  estado_eleicao VARCHAR(200) NULL DEFAULT NULL,
+  pais_eleicao VARCHAR(200) NULL DEFAULT NULL,
   cep_eleicao VARCHAR(8) NOT NULL,
   endereco_eleicao VARCHAR(200) NOT NULL,
   data_inicio_eleicao DATE NOT NULL,
   data_fim_eleicao DATE NOT NULL,
-  instituição_id_instituicao INT NOT NULL,
+  instituicao_id_instituicao INT(11) NOT NULL,
   PRIMARY KEY (id_eleicao),
-  CONSTRAINT fk_eleicao_instituição1
-    FOREIGN KEY (instituição_id_instituicao)
-    REFERENCES instituição (id_instituicao)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  CONSTRAINT fk_eleicao_instituicao
+    FOREIGN KEY (instituicao_id_instituicao)
+    REFERENCES eleicaoabp.instituicao (id_instituicao))
 
 
-
-CREATE TABLE candidato (
-  id_candidato INT NOT NULL,
+CREATE TABLE eleicaoabp.candidato (
+  id_candidato INT(11) NOT NULL,
   cpf_candidato VARCHAR(11) NOT NULL,
   nome_candidato VARCHAR(100) NOT NULL,
-  rg_candidato VARCHAR(7) NULL,
+  rg_candidato VARCHAR(9) NULL DEFAULT NULL,
   data_nasc_candidato DATE NOT NULL,
-  sexo_candidato CHAR(1) NULL,
+  sexo_candidato CHAR(1) NULL DEFAULT NULL,
   zona_candidato VARCHAR(200) NOT NULL,
-  cidade_candidato VARCHAR(200) NULL,
-  bairro_candidato VARCHAR(200) NULL,
-  endereço_candidato VARCHAR(200) NULL,
+  cidade_candidato VARCHAR(200) NULL DEFAULT NULL,
+  bairro_candidato VARCHAR(200) NULL DEFAULT NULL,
+  endereco_candidato VARCHAR(200) NULL DEFAULT NULL,
   nacionalidade_candidato VARCHAR(100) NOT NULL,
-  mae_candidato VARCHAR(100) NULL,
-  pai_candidato VARCHAR(100) NULL,
+  mae_candidato VARCHAR(100) NULL DEFAULT NULL,
+  pai_candidato VARCHAR(100) NULL DEFAULT NULL,
   cep_candidato VARCHAR(8) NOT NULL,
-  eleicao_id_eleicao INT NOT NULL,
-  PRIMARY KEY (id_candidato),
-  CONSTRAINT fk_candidato_eleicao1
+  eleicao_id_eleicao INT(11) NOT NULL,
+  cd_voto_candidato INT NULL,
+  PRIMARY KEY (id_candidato, cd_voto_candidato),
+  CONSTRAINT fk_candidato_eleicao
     FOREIGN KEY (eleicao_id_eleicao)
-    REFERENCES eleicao (id_eleicao)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    REFERENCES eleicaoabp.eleicao (id_eleicao))
 
 
 
-CREATE TABLE eleitor (
-  id_eleitor INT NOT NULL,
+CREATE TABLE eleicaoabp.eleitor (
+  id_eleitor INT(11) NOT NULL,
   cpf_eleitor VARCHAR(11) NOT NULL,
   nome_eleitor VARCHAR(45) NOT NULL,
-  rg_eleitor VARCHAR(7) NOT NULL,
-  data_nasc_eleitor VARCHAR(11) NOT NULL,
-  sexo_eleitor CHAR(1) NULL,
+  rg_eleitor VARCHAR(9) NOT NULL,
+  data_nasc_eleitor DATE NOT NULL,
+  sexo_eleitor CHAR(1) NULL DEFAULT NULL,
   numero_eleitor VARCHAR(45) NOT NULL,
   zona_eleitor VARCHAR(45) NOT NULL,
-  cidade_eleitor VARCHAR(200) NULL,
-  bairro_eleitor VARCHAR(200) NULL,
-  endereço_eleitor VARCHAR(200) NULL,
+  cidade_eleitor VARCHAR(200) NULL DEFAULT NULL,
+  bairro_eleitor VARCHAR(200) NULL DEFAULT NULL,
+  endereco_eleitor VARCHAR(200) NULL DEFAULT NULL,
   nacionalidade_eleitor VARCHAR(200) NOT NULL,
-  mae_eleitor VARCHAR(100) NULL,
-  pai_eleitor VARCHAR(100) NULL,
-  cep_eleitor VARCHAR(8) NOT NULL,
-  PRIMARY KEY (id_eleitor))
+  mae_eleitor VARCHAR(100) NULL DEFAULT NULL,
+  pai_eleitor VARCHAR(100) NULL DEFAULT NULL,
+  cep_eleitor VARCHAR(8) NOT NULL,)
 
 
 
-CREATE TABLE voto (
-  id_voto INT NOT NULL,
+CREATE TABLE eleicaoabp.voto (
+  id_voto INT(11) NOT NULL,
   tipo_voto VARCHAR(45) NOT NULL,
   data_voto VARCHAR(45) NOT NULL,
-  candidato_id_candidato INT NOT NULL,
-  eleicao_id_eleicao INT NOT NULL,
-  eleitor_id_eleitor INT NOT NULL,
+  eleitor_id_eleitor INT(11) NOT NULL,
+  candidato_id_candidato INT(11) NOT NULL,
+  candidato_cd_voto_candidato INT NOT NULL,
   PRIMARY KEY (id_voto),
-  CONSTRAINT fk_voto_candidato1
-    FOREIGN KEY (candidato_id_candidato)
-    REFERENCES candidato (id_candidato)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_voto_eleicao1
-    FOREIGN KEY (eleicao_id_eleicao)
-    REFERENCES eleicao (id_eleicao)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_voto_eleitor1
+  CONSTRAINT fk_voto_eleitor
     FOREIGN KEY (eleitor_id_eleitor)
-    REFERENCES eleitor (id_eleitor)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-
+    REFERENCES eleicaoabp.eleitor (id_eleitor)
+  CONSTRAINT fk_voto_candidato1
+    FOREIGN KEY (candidato_id_candidato , candidato_cd_voto_candidato)
+    REFERENCES eleicaoabp.candidato (id_candidato , cd_voto_candidato))
