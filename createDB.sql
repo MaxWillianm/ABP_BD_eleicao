7de6e4a181f4002e1ed6d92ced817c9f025aa285
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-06-30 05:12:33 -0300

    deu certo

--- createDB.sql
+++ createDB.sql
@@ -1,10 +1,8 @@
--- Rodar primeiro esse comando sozinho
---CREATE DATABASE eleicaoABP
+CREATE DATABASE BD_eleicao_ABP16
+USE BD_eleicao_ABP16 ;
 
--- Depois esse
--- USE eleicaoABP
 
-CREATE TABLE instituicao (
+CREATE TABLE instituição (
   id_instituicao INT NOT NULL,
   cep_instituicao VARCHAR(8) NULL,
   cidade_instituicao VARCHAR(200) NULL,
@@ -15,6 +13,8 @@ CREATE TABLE instituicao (
   nome_fantasia_instituicao VARCHAR(200) NULL,
   cnpj_instituicao VARCHAR(14) NULL,
   PRIMARY KEY (id_instituicao))
+
+
 
 CREATE TABLE eleicao (
   id_eleicao INT NOT NULL,
@@ -27,43 +27,42 @@ CREATE TABLE eleicao (
   endereco_eleicao VARCHAR(200) NOT NULL,
   data_inicio_eleicao DATE NOT NULL,
   data_fim_eleicao DATE NOT NULL,
-  instituicao_id_instituicao INT NOT NULL,
+  instituição_id_instituicao INT NOT NULL,
   PRIMARY KEY (id_eleicao),
-  CONSTRAINT fk_eleicao_instituicao
-    FOREIGN KEY (instituicao_id_instituicao)
-    REFERENCES instituicao (id_instituicao)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  CONSTRAINT fk_eleicao_instituição1
+    FOREIGN KEY (instituição_id_instituicao)
+    REFERENCES instituição (id_instituicao))
+
 
 CREATE TABLE candidato (
   id_candidato INT NOT NULL,
   cpf_candidato VARCHAR(11) NOT NULL,
   nome_candidato VARCHAR(100) NOT NULL,
-  rg_candidato VARCHAR(9) NULL,
+  rg_candidato VARCHAR(7) NULL,
   data_nasc_candidato DATE NOT NULL,
   sexo_candidato CHAR(1) NULL,
   zona_candidato VARCHAR(200) NOT NULL,
   cidade_candidato VARCHAR(200) NULL,
   bairro_candidato VARCHAR(200) NULL,
-  endereco_candidato VARCHAR(200) NULL,
+  endereço_candidato VARCHAR(200) NULL,
   nacionalidade_candidato VARCHAR(100) NOT NULL,
   mae_candidato VARCHAR(100) NULL,
   pai_candidato VARCHAR(100) NULL,
   cep_candidato VARCHAR(8) NOT NULL,
   eleicao_id_eleicao INT NOT NULL,
   PRIMARY KEY (id_candidato),
-  CONSTRAINT fk_candidato_eleicao
+  CONSTRAINT fk_candidato_eleicao1
     FOREIGN KEY (eleicao_id_eleicao)
-    REFERENCES eleicao (id_eleicao)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    REFERENCES eleicao (id_eleicao))
+
+
 
 CREATE TABLE eleitor (
   id_eleitor INT NOT NULL,
   cpf_eleitor VARCHAR(11) NOT NULL,
   nome_eleitor VARCHAR(45) NOT NULL,
-  rg_eleitor VARCHAR(9) NOT NULL,
-  data_nasc_eleitor DATE NOT NULL,
+  rg_eleitor VARCHAR(7) NOT NULL,
+  data_nasc_eleitor VARCHAR(11) NOT NULL,
   sexo_eleitor CHAR(1) NULL,
   numero_eleitor VARCHAR(45) NOT NULL,
   zona_eleitor VARCHAR(45) NOT NULL,
@@ -76,26 +75,21 @@ CREATE TABLE eleitor (
   cep_eleitor VARCHAR(8) NOT NULL,
   PRIMARY KEY (id_eleitor))
 
+
 CREATE TABLE voto (
   id_voto INT NOT NULL,
   tipo_voto VARCHAR(45) NOT NULL,
   data_voto VARCHAR(45) NOT NULL,
-  candidato_id_candidato INT NULL,
+  candidato_id_candidato INT NOT NULL,
   eleicao_id_eleicao INT NOT NULL,
   eleitor_id_eleitor INT NOT NULL,
   PRIMARY KEY (id_voto),
-  CONSTRAINT fk_voto_candidato
+  CONSTRAINT fk_voto_candidato1
     FOREIGN KEY (candidato_id_candidato)
-    REFERENCES candidato (id_candidato)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_voto_eleicao
+    REFERENCES candidato (id_candidato),
+  CONSTRAINT fk_voto_eleicao1
     FOREIGN KEY (eleicao_id_eleicao)
-    REFERENCES eleicao (id_eleicao)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_voto_eleitor
+    REFERENCES eleicao (id_eleicao),
+  CONSTRAINT fk_voto_eleitor1
     FOREIGN KEY (eleitor_id_eleitor)
-    REFERENCES eleitor (id_eleitor)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    REFERENCES eleitor (id_eleitor))
